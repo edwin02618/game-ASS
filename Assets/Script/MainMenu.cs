0d53d7b8ffa259ee89c86d996bdc60149bcd49b2
edwin02618 <edwin02618@gmail.com>
2022-10-26 19:06:19 +1100

    fixed

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public void OnsceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if ((int)mode + 1 == scene.buildIndex)
+        {
+            GameObject exit = GameObject.FindWithTag("Exit");
+            Button button = exit.GetComponent<Button>();
+            button.onClick.AddListener(Exit);
+        }
+    }
     public void Level1()
     {
+        DontDestroyOnLoad(gameObject);
         SceneManager.LoadScene("SampleScene");
+        SceneManager.sceneLoaded += OnsceneLoaded;
+    }
+   public void Exit()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("StartScene");
     }
 }
